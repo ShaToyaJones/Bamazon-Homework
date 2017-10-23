f529b2c261ea79a8ad1b071fd42739adf6968383
ShaToyaJones <jshatoyaj@yahoo.com>
2017-10-22 21:06:48 -0400

    added dummy data to database.

--- bamazon.sql
+++ bamazon.sql
@@ -5,11 +5,14 @@ USE bamazonDB;
 
 CREATE TABLE products (
   item_id INT NOT NULL,
-  product_name VARCHAR(255) NULL,
-  department_name VARCHAR(255) NULL,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
   price DECIMAL(10,2) NULL,
   stock_quantity INT NULL,
   PRIMARY KEY (item_id)
 );
 
 SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("laptop", "electronics", 650.00, 3), ("all in one computer", "electronics", 1920.00, 9), ("cell phone", "electronics", 920.00, 53), ("tuna", "grocery", 1.57, 999 ), ("black beans", "grocery", .99, 333), ("corn bread", "grocery", 2.59, 137), ("guitar", "music instruments", 249.99, 29), ("drums", "music instruments", 349.99, 9), ("piano", "music instruments", 23999.00, 3) ("microphone", "music instruments", 99.00, 24);
